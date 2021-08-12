730aca9f56c1a4e19f3fdb3c6e46ae5f320248f8
Sushil0394 <mahatosushil94@gmail.com>
2021-08-12 17:53:59 +0530

    Added application logic

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      weather: '',
+    };
+  }
+
+  getWeather = async () => {
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139';
+    return fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({
+          weather: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error('Error while fetching weather Report: ' + error);
+      });
+  };
+
+  componentDidMount = () => {
+    this.getWeather();
+  }
+
+  render() {
+    if (this.state.weather === '') {
+      return (
+        <View style={{}}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.subContainer}>
+            <Text style={styles.title}>Weather Forcast</Text>
+            <Image style={styles.cloudImage} source={require('./WeatherForcastLogo.jpg')}></Image>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={{fontSize:20}}>Temprature: {this.state.weather.main.temp}&deg; C </Text>
+            <Text style={{fontSize:20}}>Humidity:  {this.state.weather.main.humidity} </Text>
+            <Text style={{fontSize:20}}>Today's Details: {this.state.weather.weather[0].description} </Text>
+          </View>
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  subContainer:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    backgroundColor: 'white',
+    padding: 20,
+
+  },
+  textContainer: {
+    backgroundColor: 'white',
+  }
 });
